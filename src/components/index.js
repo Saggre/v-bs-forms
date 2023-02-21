c956b5c3604bc87eb1983c576cf122f01d6ff630
Sakri Koskimies <sakri.koskimies@hotmail.com>
2023-02-21 20:14:12 +0200

    Update build

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Form } from './Form.vue';
+export { default as BaseForm } from './BaseForm.vue';
+export { default as FormFieldError } from './fields/standard/InputError.vue';
+export { default as FormFieldLabel } from './fields/standard/Label.vue';
+export { default as BaseFormField } from './fields/BaseFormField.vue';
+export { default as CardFormField } from './fields/CardFormField.vue';
+//# sourceMappingURL=index.js.map

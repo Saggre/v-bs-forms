0d56b1f2b10c757b68f569912d165c0397b7fdf3
Sakri Koskimies <sakri.koskimies@hotmail.com>
2023-02-21 20:39:32 +0200

    Include vue components in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,19 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['tests/unit/setup.ts'],
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'v-bs-forms',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
 });

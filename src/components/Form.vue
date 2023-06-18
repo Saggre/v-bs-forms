52a24d07517dcda1fd0a21bfa12e427c9587e047
Sakri Koskimies <sakri.koskimies@hotmail.com>
2023-06-19 00:27:20 +0300

    Fix possibly undefined value

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -11,7 +11,7 @@
         v-for="(field, key) in form.fields"
         :key="key"
         :class="
-          field.columnClass ?? {
+          field?.columnClass ?? {
             'col-12': true,
           }
         "
